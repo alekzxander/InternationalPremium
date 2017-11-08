fe6d3ed3b7c13c9ce4ecb1c6b07b4b2572a33d90
LauGR <laurent.gregoire974@gmail.com>
2017-11-08 10:27:49 +0400

    ok

--- app/routes.js
+++ app/routes.js
@@ -9,27 +9,27 @@ module.exports = function (app, passport) {
 
 
     // normal routes ===============================================================
-    app.get('/dashbord', permissions.can('access admin page'), (req, res) => {
+    app.get('/dashbord', (req, res) => {
         res.render('dashbord.ejs')
 
     })
-    app.get('/card/:id/delete', permissions.can('access admin page'), (req, res) => {
+    app.get('/card/:id/delete', (req, res) => {
         voyage.remove({ _id: req.params.id }, (err, delData) => {
             res.render("validation.ejs");
         })
     })
-    app.get('/dashbord/card', permissions.can('access admin page'), (req, res) => {
+    app.get('/dashbord/card', (req, res) => {
         voyage.find((err, carte) => {
             res.render('card.ejs', { cartes: carte });
         });
     });
 
-    app.get('/dashbord/dashItineraire/', permissions.can('access admin page'), (req, res) => {
+    app.get('/dashbord/dashItineraire/', (req, res) => {
         voyage.find((err, voyages) => {
             res.render('dashItineraire.ejs', { voyages: voyages })
         });
     })
-    app.get('/ajoutLieux/:id', permissions.can('access admin page'), (req, res) => {
+    app.get('/ajoutLieux/:id', (req, res) => {
         voyage.find((err, voyages) => {
             res.render('ajoutLieux.ejs', {
                 id: req.params.id, mesVoyages: voyages.filter((voyage) => {
@@ -38,7 +38,7 @@ module.exports = function (app, passport) {
             })
         });
     })
-    app.post('/ajoutLieux/:id', permissions.can('access admin page'), upload.single('img'), (req, res) => {
+    app.post('/ajoutLieux/:id', upload.single('img'), (req, res) => {
         let fileToUpload = req.file;
         let target_path = 'public/images/' + fileToUpload.originalname;
         let tmp_path = fileToUpload.path;
@@ -71,7 +71,7 @@ module.exports = function (app, passport) {
 
     // create card
     // process the card form
-    app.post('/dashbord/card', permissions.can('access admin page'), upload.single('img'), (req, res) => {
+    app.post('/dashbord/card', upload.single('img'), (req, res) => {
         /** The original name of the uploaded file
          stored in the variable "originalname". **/
         var fileToUpload = req.file;
@@ -111,7 +111,7 @@ module.exports = function (app, passport) {
     });
 
     /* update card */
-    app.get('/updatecard/:id', permissions.can('access admin page'), (req, res) => {
+    app.get('/updatecard/:id', (req, res) => {
         voyage.find((err, voyages) => {
             res.render("updatecard.ejs", {
                 voyage: req.params.id, card: voyages.filter((voyage) => {
@@ -121,10 +121,10 @@ module.exports = function (app, passport) {
         })
     })
 
-    app.post('/updatecard/:id', permissions.can('access admin page'), upload.single('img'), (req, res) => {
-        var fileToUpload = req.file;
-        var target_path = 'public/images/' + fileToUpload.originalname;
-        var tmp_path = fileToUpload.path;
+    app.post('/updatecard/:id', upload.single('img'), (req, res) => {
+        // var fileToUpload = req.file;
+        // var target_path = 'public/images/' + fileToUpload.originalname;
+        // var tmp_path = fileToUpload.path;
 
         voyage.findByIdAndUpdate(req.params.id, {
             $set: {
@@ -134,20 +134,20 @@ module.exports = function (app, passport) {
                 sejour: req.body.sejour,
                 preview: req.body.preview,
                 text: req.body.text,
-                img: fileToUpload.originalname
+                // img: fileToUpload.originalname
             }
         },
             { new: true },
             (err, voyage) => {
                 voyage.save()
                     .then(item => {
-                        var src = fs.createReadStream(tmp_path);
-                        var dest = fs.createWriteStream(target_path);
-                        src.pipe(dest);
+                        // var src = fs.createReadStream(tmp_path);
+                        // var dest = fs.createWriteStream(target_path);
+                        // src.pipe(dest);
                         res.redirect("/dashbord/card");
-                        fs.unlink(tmp_path);
-                        src.on('end', function () { res.redirect("/dashbord/card"); });
-                        src.on('error', function (err) { res.render('error'); });
+                        // fs.unlink(tmp_path);
+                        // src.on('end', function () { res.redirect("/dashbord/card"); });
+                        // src.on('error', function (err) { res.render('error'); });
 
                     })
                     .catch(err => {
