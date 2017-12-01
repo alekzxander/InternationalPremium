434ad8e87ef55fd1f4092634b1e42a1e213c8ead
smile <smile@localhost.localdomain>
2017-12-01 14:27:02 +0400

    final commit

--- app/models/user.js
+++ app/models/user.js
@@ -25,19 +25,19 @@ var userSchema = mongoose.Schema({
 });
 
 // generating a hash
-userSchema.methods.generateHash = function(password) {
+userSchema.methods.generateHash = function (password) {
     return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
 };
 
 // checking if password is valid
-userSchema.methods.validPassword = function(password) {
+userSchema.methods.validPassword = function (password) {
     return bcrypt.compareSync(password, this.local.password);
 };
 
-userSchema.methods.isMember = function() {
+userSchema.methods.isMember = function () {
     return (this.role === "member");
 };
-userSchema.methods.isAdmin = function() {
+userSchema.methods.isAdmin = function () {
     return (this.role === "admin");
 };
 
